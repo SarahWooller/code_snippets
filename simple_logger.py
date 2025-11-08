2494b81968a8da233859be61a818d889f2f2570f
SarahWooller <SarahWooller@users.noreply.github.com>
2025-11-08 20:48:57 +0000

    A simple logger that logs to path.log and prints errors to console.

--- simple_logger.py
+++ simple_logger.py
@@ -1,9 +1,16 @@
+"""Simply sets up a basic logger. By default it will setup the log as path.log in the same directory as
+  path.py - the python file calling the logger"""
 
 import logging
+import inspect
 
-def simple_logger(log_filename="test.log"):
+def simple_logger(log_filename=""):
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
+    # set up the path to the filehandler
+    if not log_filename:
+        caller_filename = [f.filename for f in inspect.stack()][-1]
+        log_filename = f"{caller_filename[:-3]}.log"
     fh = logging.FileHandler(log_filename) # file handler for all messages
     fh.setLevel(logging.DEBUG)
     ch = logging.StreamHandler() # console handler for errors
@@ -15,4 +22,4 @@ def simple_logger(log_filename="test.log"):
     logger.addHandler(fh)
     return logger
 
-logger = simple_logger()
+
